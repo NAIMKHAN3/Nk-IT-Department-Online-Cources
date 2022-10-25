b9b7e0b76d508b30fea65d7d3790023a14efb64b
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-10-25 13:17:56 +0600

    navber added

---
+++ src/components/Navbar/Navber.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Navber = () => {
+    return (
+        <Navbar bg="dark" variant='dark' expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">NK-IT-Department</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Courses</Nav.Link>
+                        <Nav.Link href="#link">Blog</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Navber;
