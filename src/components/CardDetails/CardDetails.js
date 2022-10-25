b2b44092a9152137a582c474b6ddf2b37cc5d254
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-10-26 00:21:41 +0600

    card details added

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
-import Cetegory from '../Cetegory/Cetegory';
+import { FaStar } from 'react-icons/fa';
 
 const CardDetails = () => {
     const course = useLoaderData()
-    const { title, image } = course;
+    const { title, image, description, price, rating } = course;
     console.log(course)
     return (
         <Container>
-
+            <Row>
+                <Col>
+                    <Card className='text-center bg-dark shadow-lg mt-4'>
+                        <Card.Title className='text-warning fs-1 my-3'>{title}</Card.Title>
+                        <Card.Img className='w-50 mx-auto' variant="top" src={image} />
+                        <Card.Body className='text-light'>
+                            <Card.Text>
+                                {
+                                    description
+                                }
+                            </Card.Text>
+                            <div className='d-flex justify-content-evenly'>
+                                <h4>Price: $ {price}</h4>
+                                <h4>Ratings: <FaStar className='text-warning'></FaStar><FaStar className='text-warning'></FaStar><FaStar className='text-warning'></FaStar>  {rating}</h4>
+                            </div>
+                            <Button className='w-50 p-3 m-3 fs-5 text-light' variant="outline-primary">Buy Now</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </Container>
     );
 };
